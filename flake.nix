1a29902cd24aab7ff6a4bd50e26cd8bcae263e6f
ggobbe <ggobbe@users.noreply.github.com>
2024-02-29 11:28:14 +0100

    dev profile

---
+++ flake.nix
@@ -0,0 +1,58 @@
+# global flake.nix
+
+{
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs"; };
+
+  outputs = { self, nixpkgs }: {
+    formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixfmt;
+    packages."aarch64-darwin".default =
+      let pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+      in pkgs.buildEnv {
+        name = "home-packages";
+        paths = with pkgs; [
+
+          # general tools
+          git
+          ffmpeg
+          curl
+          wget
+          jq
+          gnused
+          ripgrep
+          zellij
+          pandoc
+          helix
+          aerc
+          btop
+          jq
+
+          # dev tools
+          kubectl
+          kubectx
+          #lima
+          protobuf
+          buf
+          mongodb-tools
+          okteto
+          docker
+          dive
+          biome
+
+          # infrastructure
+          k9s
+
+          # programming environments
+          deno
+          bun
+          nodejs
+          yarn
+          go
+          erlang_26
+          gleam
+          rebar3
+          elixir_1_16
+          elixir-ls
+        ];
+      };
+  };
+}
